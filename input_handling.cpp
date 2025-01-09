39fcb6074b995f909470fb0231afc95a08e89aae
abratosin <andreibratosin2004@gmail.com>
2025-01-09 19:56:48 +0200

    refactored input_handling to header file

--- input_handling.cpp
+++ input_handling.cpp
@@ -10,98 +10,89 @@
 #include <sstream>
 
 using namespace std;
-class CSVInputHandler{
-        private:
-            string file_path;
-            // în obiectul de input în sine păstrez liniile din fișier, nu datele
-            // așa (mi se pare că) am mai mult control asupra modului în care datele sunt scrise în fișier
-            vector<string> lines;
+CSVInputHandler::CSVInputHandler(const string &file_path_in){
+    this->file_path = file_path_in;
 
-        // în principiu, clasa CSVInputHandler este singura care citește din fișier în mod direct, și stochează datele sub formă de vector al liniilor
-        // apoi clasa oferă această informație restului programului fie ca atare, fie sub formă de vector de vectori de stringuri
-        // deci majoritatea claselor vor primi acel vector de date, vor face procesările lor, îl vor returna obiectului de CSV, care va scrie datele în fișierul dorit
-        public:
+    read_lines();
+}
 
-        CSVInputHandler(const string &file_path_in){
-            this->file_path = file_path_in;
-
-            read_lines();
+CSVInputHandler::~CSVInputHandler(){
+    write_lines();
+}
+void CSVInputHandler::read_lines(){
+    ifstream file(file_path);
+    getline(file, csv_header);
+    vector<string> lines_temp;
+    if(file.is_open()){
+        string line;
+        while(getline(file, line)){
+            lines_temp.push_back(line);
         }
-        ~CSVInputHandler(){
-            write_lines();
+    }
+
+    lines = lines_temp;
+}
+
+void CSVInputHandler::write_lines(){
+    ofstream file;
+    file.open(file_path);
+    file<<csv_header<<endl;
+    cout<<csv_header<<endl;
+    if(file.is_open()){
+        for(int i=0; i<lines.size(); i++){
+            file<<lines[i]<<endl;
+            cout<<lines[i]<<endl;
         }
-        void read_lines(){
-            ifstream file(file_path);
-            vector<string> lines_temp;
-            if(file.is_open()){
-                string line;
-                while(getline(file, line)){
-                    lines_temp.push_back(line);
-                }
-            }
+    }
+}
 
-            lines = lines_temp;
+void CSVInputHandler::print_lines(){
+    for(int i=0; i<lines.size(); i++){
+        cout<<lines[i]<<endl;
+    }
+}
+
+vector<string> CSVInputHandler::get_lines(){
+    return lines;
+}
+
+
+// este mai ușor și ceva mai safe doar să comunic cu alte clase de parsing prin data, cum fiecare clasă în parte nu ar trebui să gestioneze decât transpunerea fiecărui câmp al clasei în string
+// așa ar trebui să nu fiu nevoit să refolosesc cod
+vector<vector<string>> CSVInputHandler::get_data(){
+    vector<vector<string>> data;
+    for(int i=0; i<lines.size(); i++){
+        vector<string> row;
+        stringstream ss(lines[i]);
+        string item;
+        while(getline(ss, item, ',')){
+            row.push_back(item);
         }
+        data.push_back(row);
+    }
+    return data;
+}
 
-        void write_lines(){
-            ofstream file;
-            file.open(file_path);
-            if(file.is_open()){
-                for(int i=0; i<lines.size(); i++){
-                    file<<lines[i]<<endl;
-                }
-            }
+// am nevoie de funcția asta pentru a putea scrie datele în fișier
+void CSVInputHandler::lines_from_data(vector<vector<string>> data){
+    vector<string> lines_temp;
+    for(int i=0; i<data.size(); i++){
+        lines_temp.push_back(line_from_data(data[i]));
+    }
+    lines = lines_temp;
+}
+
+string CSVInputHandler::line_from_data(const vector<string> &data){
+    string line = "";
+    for(int i=0; i<data.size(); i++){
+        line += data[i];
+        if(i<data.size()-1){
+            line += ",";
         }
+    }
+    return line;
+}
 
-        void print_lines(){
-            for(int i=0; i<lines.size(); i++){
-                cout<<lines[i]<<endl;
-            }
-        }
-
-        vector<string> get_lines(){
-            return lines;
-        }
-
-
-        // este mai ușor și ceva mai safe doar să comunic cu alte clase de parsing prin data, cum fiecare clasă în parte nu ar trebui să gestioneze decât transpunerea fiecărui câmp al clasei în string
-        // așa ar trebui să nu fiu nevoit să refolosesc cod
-        vector<vector<string>> get_data(){
-            vector<vector<string>> data;
-            for(int i=0; i<lines.size(); i++){
-                vector<string> row;
-                stringstream ss(lines[i]);
-                string item;
-                while(getline(ss, item, ',')){
-                    row.push_back(item);
-                }
-                data.push_back(row);
-            }
-            return data;
-        }
-
-        // am nevoie de funcția asta pentru a putea scrie datele în fișier
-        void lines_from_data(vector<vector<string>> data){
-            vector<string> lines_temp;
-            for(int i=0; i<data.size(); i++){
-                lines_temp.push_back(line_from_data(data[i]));
-            }
-            lines = lines_temp;
-        }
-
-        string line_from_data(const vector<string> &data){
-            string line = "";
-            for(int i=0; i<data.size(); i++){
-                line += data[i];
-                if(i<data.size()-1){
-                    line += ",";
-                }
-            }
-            return line;
-        }
-
-        void append_line(const string& line){
-            lines.push_back(line);
-        }
-
-};
+void CSVInputHandler::append_line(const string& line){
+    lines.push_back(line);
+}
